daa1570ba1483b27bb6611e131a237d17d8dab32
daniel_herraiz <daniel.herraiz.tello@gmail.com>
2025-04-23 02:09:40 +0200

    Ajustes Incremento y formato precios

--- backup.py
+++ backup.py
@@ -13,7 +13,6 @@ import time
 # import os
 # import json
 
-
 # from RetoExtraSites.CasaDelLibro import getPrice_casaDelLibro
 
 st.set_page_config(
@@ -26,18 +25,16 @@ st.set_page_config(
 st.title("PC2 Reto Extra")
 st.header("Daniel Herraiz")
 
-# st.subheader("Casa Del Libro")
-# st.write(getPrice_casaDelLibro())
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.firefox.options import Options
 
 import time
 import os
+
+import traceback
 
 
 
@@ -48,29 +45,7 @@ options = Options()
 #hide firefox window
 # os.environ['MOZ_HEADLESS'] = '1'
 
-def add_increment_column(df):
-    if df.empty:
-        df["Increment"] = []
-        return df
 
-    # Clean and convert 'Current Price' to float
-    df["Current Price"] = (
-        df["Current Price"]
-        .astype(str)
-        .str.replace(r"[^\d.,]", "", regex=True)  # Remove currency symbols or other chars
-        .str.replace(",", ".")                    # Handle decimal commas 
-        .astype(float)
-    )
-
-    min_price = df["Current Price"].min()
-
-    df["Increment"] = ((df["Current Price"] - min_price) / min_price * 100).round(2).astype(str) + " %"
-    
-    # Back to string + € / %
-    # df["Increment"] = df["Increment"].astype(str) + " %"
-    df["Current Price"] = df["Current Price"].round(2).astype(str) + " €"
-    
-    return df
 
 def getBookResults(query, bookLimit):
     driver = webdriver.Firefox(service=service, options=options)
@@ -94,7 +69,7 @@ def getBookResults(query, bookLimit):
 
     root1 = driver.find_element(By.XPATH, "//div[@class='x-root-container']")
     shadow_root1 = expand_shadow_element(root1)
-
+    time.sleep(0.5)
     # Interact with right side panel ('Ordenar por)
     # Open right side panel 
     rightPanel = shadow_root1.find_element(By.CSS_SELECTOR, 'button[data-test="toggle-facets-button"]')
@@ -109,7 +84,6 @@ def getBookResults(query, bookLimit):
             checkbox.click()
     time.sleep(2)
     # Availability dropdown
-    # availability = shadow_root1.find_element(By.CSS_SELECTOR, 'div[data-test="availability"]')
     try:
         availability = shadow_root1.find_element(By.CSS_SELECTOR, 'div[data-test="availability"]')
         
@@ -123,19 +97,9 @@ def getBookResults(query, bookLimit):
                 print('click')
                 break
         time.sleep(1.5)
-    except NoSuchElementException:
-        print('Filtro de disponibilidad no encontrado')
+    except (NoSuchElementException , TimeoutException) as e:
+        print(e.args)
          
-    # --- FIND ALL BOOK RESULT CARDS ---
-    ###########################
-    #     # --- EXPAND SHADOW DOM ROOT ---
-    # def expand_shadow_element(element):
-    #     shadow_root = driver.execute_script("return arguments[0].shadowRoot", element)
-    #     return shadow_root
-
-    # root1 = driver.find_element(By.XPATH, "//div[@class='x-root-container']")
-    # shadow_root1 = expand_shadow_element(root1)
-    # ##########################
     results = shadow_root1.find_elements(By.CSS_SELECTOR, "li.x-base-grid__item")
     books = []
     bookDf = pd.DataFrame()
@@ -158,7 +122,8 @@ def getBookResults(query, bookLimit):
             try:
                 detail_el = article.find_elements(By.CSS_SELECTOR, 'span.x-text1.x-text1-sm.x-font-bold.x-text-lead-50')
                 detail = detail_el[0].text.strip() if detail_el else "N/A"
-            except NoSuchElementException:
+            except (NoSuchElementException , TimeoutException) as e:
+                print(e.args)
                 detail = "N/A"
 
             # Price (Current)
@@ -168,7 +133,8 @@ def getBookResults(query, bookLimit):
                 if not current_price:
                     print('Exception price empty 1')
                     continue
-            except NoSuchElementException:
+            except (NoSuchElementException , TimeoutException) as e:
+                print(e.args)
                 print('Exception no price found 1')
                 continue
 
@@ -176,14 +142,16 @@ def getBookResults(query, bookLimit):
             try:
                 original_price_elem = article.find_element(By.CSS_SELECTOR, 'div[data-test="result-previous-price"]')
                 original_price = original_price_elem.text.strip()
-            except NoSuchElementException:
+            except (NoSuchElementException , TimeoutException) as e:
+                print(e.args)
                 original_price = current_price
 
             # Image
             try:
                 img_elem = article.find_element(By.CSS_SELECTOR, 'img[data-test="result-picture-image"]')
                 img_url = img_elem.get_attribute("src")
-            except NoSuchElementException:
+            except (NoSuchElementException , TimeoutException) as e:
+                print(e.args)
                 img_url = "N/A"
 
             #Link
@@ -195,14 +163,14 @@ def getBookResults(query, bookLimit):
                 link = "N/A"
 
             books.append({
-                "Title": title,
-                "Author": author,
-                "Detail": detail,
-                "Original Price": original_price,
-                "Current Price": current_price,
-                "Image url": img_url,
-                "Link url": link,
-                "Store": "Casa del libro"
+                "Título": title,
+                "Autor": author,
+                "Detalle": detail,
+                "Precio base": original_price,
+                "Precio final": current_price,
+                "Cubierta": img_url,
+                "Enlace": link,
+                "Tienda": "Casa del libro"
             })
             print(len(books))
             bookDf = pd.DataFrame(books)
@@ -268,14 +236,14 @@ def getBookResults2(query, bookLimit):
                     link = "N/A"
 
                 books.append({
-                    "Title": title,
-                    "Author": author,
-                    "Detail": detail,
-                    "Original Price": original_price,
-                    "Current Price": current_price,
-                    "Image url": img_url,
-                    "Link url": link,
-                    "Store": "Librería central"
+                    "Título": title,
+                    "Autor": author,
+                    "Detalle": detail,
+                    "Precio base": original_price,
+                    "Precio final": current_price,
+                    "Cubierta": img_url,
+                    "Enlace": link,
+                    "Tienda": "Librería central"
                 })
                 print(len(books))
                 bookDf = pd.DataFrame(books)
@@ -352,81 +320,91 @@ def getBookResults2(query, bookLimit):
 #             print(f"Error al obtener resultados: {e}")
 #     return bookDf.truncate(after=bookLimit)
 
+def add_increment_column(df, min_price):
+    try:
+        if df.empty:
+            df["Incremento %"] = []
+            return df
+        if(min_price==-1):
+            min_price = df["Precio final"].min()
+        
+        df["Incremento %"] = ((df["Precio final"] - min_price) / min_price * 100).round(2).astype(str) + " %"
+        df["Precio base"] = (df["Precio base"].str.replace(",", "."))
+        df["Precio final"] = df["Precio final"].round(2).astype(str) + " €"
+    except Exception as e:
+        traceback.print_exc()
+    return df, min_price
 
 def drawDfTable(inputDf,dfkey):
 
     st.data_editor(
         inputDf,
         use_container_width=True,
+        column_order=['Título','Autor','Detalle','Precio base','Precio final','Incremento %','Cubierta','Enlace'],
         column_config={
-            "Image url": st.column_config.ImageColumn(
+            "Cubierta": st.column_config.ImageColumn(
                 "Cubierta", 
                 width="small",
                 help="Doble click para ampliar"
             ),
-            "Link url": st.column_config.LinkColumn(
+            "Enlace": st.column_config.LinkColumn(
                 "Enlace", 
                 display_text=r"https://www.(.*?)\.com"
             ),
-            "Detail": st.column_config.TextColumn("Detalle"),
-            "Original Price": st.column_config.TextColumn("Precio original"),
-            "Current Price": st.column_config.TextColumn("Precio final"),
-            "Author": st.column_config.TextColumn("Autor"),
-            "Title": st.column_config.TextColumn("Título"),
-            "Increment" : st.column_config.TextColumn("Incremento %"),
             
-        },
-        # column_order=['Título','Autor','Detalle','Precio original','Precio final','Increment','Cubierta','Enlace'],
+        }, 
         key=dfkey
         # hide_index=True
     )
 
 def getSortedResults(fetch_func, query, bookLimit):
     df = fetch_func(query, bookLimit)
-    if not df.empty and 'Current Price' in df.columns:
-        return df.sort_values(by='Current Price', ignore_index=True)
+    if not df.empty and 'Precio final' in df.columns:
+        df["Precio final"] = (
+        df["Precio final"]
+        .astype(str)
+        .str.replace(r"[^\d.,]", "", regex=True)  # Remove currency symbols or other chars
+        .str.replace(",", ".")                    # Handle decimal commas 
+        .astype(float)
+    )
+        return df.sort_values(by='Precio final', ignore_index=True)
     return df
 
 def obtenerResultados(query, bookLimit):
 
     if query.strip():
-        print('INICIO____________________________________________________________________')
         with st.spinner("Buscando libros..."):
             try:
-                #Lista de funciones por cada tienda
+                #Function list for each store
                 fetchFuncs = [getBookResults, getBookResults2]
                 dfs = [getSortedResults(f, query, bookLimit) for f in fetchFuncs]
                 if not dfs:
                     st.warning("No results found.")
                 else:
-                    # If 0 or 1 result per store, draw only one table
+
                     # Draw reduced table with first result from each store
-                    # reducedBookDf = pd.concat([df.iloc[[0]] for df in dfs if not df.empty], ignore_index=True)
-                    reducedBookDf = add_increment_column(pd.concat([df.iloc[[0]] for df in dfs if not df.empty], ignore_index=True))
+                    reducedBookDf, minprice = add_increment_column(pd.concat([df.iloc[[0]] for df in dfs if not df.empty], ignore_index=True), -1)
                     st.markdown("#### 📘 Los resultados más baratos de cada tienda:")
                     drawDfTable(reducedBookDf, 'Reduced')
                     
                     # Show summary
-                    
                     st.markdown("*Resumen de resultados por tienda:*")
                     for df in dfs:
                         
                         if not df.empty:
-                            store_name = df['Store'].iloc[0]
+                            store_name = df['Tienda'].iloc[0]
                             st.write(f"  - {store_name}: {len(df)} resultados")
 
-                    # if len(dfs) > 2 or any(df.shape[0] > 1 for df in dfs):
+                    #If more than 1 result in a store, include the rest
                     if any(df.shape[0] > 1 for df in dfs) and bookLimit > 1:
                         # Draw expanded table with the rest (up to bookLimit)
                         expandedDfs = [df.iloc[1:bookLimit] for df in dfs if df.shape[0] > 1]
                         if expandedDfs:
                             st.markdown("#### 📚 Resultados adicionales por tienda:")
-                            # bookDf = pd.concat(expandedDfs, ignore_index=True).sort_values(by='Current Price', ignore_index=True)
-                            bookDf = pd.concat(expandedDfs, ignore_index=True).sort_values(by='Current Price', ignore_index=True)
+                            bookDf, minprice = add_increment_column(pd.concat(expandedDfs, ignore_index=True).sort_values(by='Precio final', ignore_index=True), minprice)
                             drawDfTable(bookDf, 'Expanded')
             except Exception as e:
                 st.error(f"An error occurred: {e}")
-        print('FIN____________________________________________________________________')
     else:
         st.warning("Please enter a book title or ISBN.")
 
@@ -441,95 +419,7 @@ with col2:
 if placeholderButton.button("🔍 Buscar"):
     # Get the current time
     current_time = datetime.now().time()
-
-    # Print the current time
     print("Current Time:", current_time)
+
     obtenerResultados(query, bookLimit)
 
-
-
-
-
-# isbn = '9788478884452'
-# url = f"https://www.casadellibro.com/?query={isbn}"
-# driver = webdriver.Firefox(service=service, options=options)
-# driver.get(url)
-
-# def expand_shadow_element(element):
-#     shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
-#     return shadow_root
-
-# root1 = driver.find_element(By.XPATH, "//div[@class='x-root-container']")
-# print(root1)
-# shadow_root1 = expand_shadow_element(root1)
-
-# try:
-#     bookPrice2 = shadow_root1.find_element(By.CSS_SELECTOR, 'div[data-test="result-current-price"]')
-#     print("Element found:", bookPrice2.text)
-# except NoSuchElementException:
-#     print("Element not found.")
-
-# print(bookPrice1[0].text)
-
-
-
-
-
-
-# from urllib.parse import urljoin
-
-# # def getLibreriaCentralResults():
-#     isbn2 = '9788478884452'
-#     url = f"https://www.casadellibro.com/?query={isbn}"
-#     print(url)
-#     page = requests.get(url)
-#     soup = BeautifulSoup(html, 'html.parser')
-#     results = []
-
-#     # Each book is within a Product schema tag
-#     products = soup.find_all('div', itemtype="http://schema.org/Product")
-
-#     for product in products:
-#         title = product.find(itemprop="name")
-#         author_meta = product.find('meta', itemprop="author")
-#         price = product.find('span', itemprop="price")
-#         currency = product.find('span', itemprop="priceCurrency")
-#         image = product.find('img', itemprop="image")
-#         isbn = product.find('meta', itemprop="isbn")
-#         url = product.find('a', itemprop="url")
-#         availability = product.find('link', itemprop="availability")
-
-#         data = {
-#             'title': title.get_text(strip=True) if title else None,
-#             'author': author_meta['content'].strip() if author_meta else None,
-#             'price': price.get_text(strip=True) if price else None,
-#             'currency': currency['content'].strip() if currency else None,
-#             'image_url': urljoin(base_url, image['src']) if image else None,
-#             'isbn': isbn['content'].strip() if isbn else None,
-#             'product_url': urljoin(base_url, url['href']) if url else None,
-#             'availability': availability['href'].split('/')[-1] if availability else None
-#         }
-
-#         results.append(data)
-
-#     return results
-#     st.data_editor(
-#         df1,
-#         use_container_width=True,
-#         column_config={
-#             "Image url": st.column_config.ImageColumn(
-#                 "Cubierta", 
-#                 width="small"
-#             ),
-#             "Link url": st.column_config.LinkColumn(
-#                 "Enlace", 
-#                 display_text=r"https://www.(.*?)\.com"
-#             ),
-#             "Detail": st.column_config.TextColumn("Detalle"),
-#             "Original Price": st.column_config.TextColumn("Precio original"),
-#             "Current Price": st.column_config.TextColumn("Precio final"),
-#             "Author": st.column_config.TextColumn("Autor"),
-#             "Title": st.column_config.TextColumn("Título")
-#         }
-#     )
-#     driver.close()
